a461791a477a76ecb8f34d86b848878a70630bdd
Syrus Akbary <me@syrusakbary.com>
2018-11-12 18:45:59 -0800

    Updated logo size

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import styles from './header.module.css'
 const Header = ({ siteTitle }) => (
   <div className="container">
     <div className={styles.header}>
-      <img src={WasmerLogo} style={{ width: 200 }} />
+      <img src={WasmerLogo} style={{ width: 200, height: 55 }} />
     </div>
   </div>
 )
