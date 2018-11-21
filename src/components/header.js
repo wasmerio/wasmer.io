22eb6d9956ee3413dab3bd96f1f16932447e292e
Syrus Akbary <me@syrusakbary.com>
2018-11-20 21:35:25 -0800

    Updated wasmer website with link to Github

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import WasmerLogo from '../images/wasmer-logo.png'
 import styles from './header.module.css'
+import { FaGithub } from 'react-icons/fa'
 
 const Header = ({ siteTitle }) => (
   <div className="container">
     <div className={styles.header}>
-      <img src={WasmerLogo} style={{ width: 200, height: 55 }} />
+      <a href="https://wasmer.io/" className={styles.logo}>
+        <img src={WasmerLogo} style={{ width: 200, height: 55 }} />
+      </a>
+      <div className={styles.navbar}>
+        <a
+          href="https://github.com/WAFoundation/wasmer"
+          className={styles.github}
+        >
+          <FaGithub className={styles.githubIcon} />
+          Github
+        </a>
+      </div>
     </div>
   </div>
 )
