a9dccf39d7fdabdff9d34365079e7f18e19cfcce
Syrus Akbary <me@syrusakbary.com>
2018-11-21 22:31:13 -0800

    Added google-analytics to the wasmer website

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,26 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-129663180-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ['/preview/**', '/do-not-track/me/too/'],
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "wasmer.io",
+      },
+    },
     'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-plugin-stylus',
