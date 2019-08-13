82a7185e391e305bc36f9eb4b331c65cc6651edc
Syrus <me@syrusakbary.com>
2019-08-12 23:28:15 -0700

    The about page is now more polished

--- pages/about.js
+++ pages/about.js
@@ -3,14 +3,20 @@ import Link from "next/link";
 import Head from "next/head";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
+import classnames from "classnames";
 
 import css from "./about.css";
+import grid from "../components/grid.css";
+import syrusPhoto from "../images/team/syrus.png";
+import ivanPhoto from "../images/team/ivan.png";
+import nickPhoto from "../images/team/nick.png";
+import aaronPhoto from "../images/team/aaron.png";
 import genericPhoto from "../images/team/generic.png";
 
 const team = [
   {
     name: "Syrus Akbary",
-    photo: genericPhoto,
+    photo: syrusPhoto,
     description:
       "Before starting Wasmer in November 2018, Syrus Akbary was the CTO of Try.com. He’s the creator of several popular Python open-source libraries like Graphene, Promise used in 5 of top 50 Y Combinator companies.",
     twitter: "syrusakbary",
@@ -18,7 +24,7 @@ const team = [
   },
   {
     name: "Ivan Enderlin",
-    photo: genericPhoto,
+    photo: ivanPhoto,
     description:
       "A versatile software engineer passionate about WebAssembly. Prior joining Wasmer he was working at Automattic and Mozilla. Ivan also created Tagua-VM a popular PHP Virtual Machine made in Rust.",
     twitter: "mnt_io",
@@ -33,16 +39,16 @@ const team = [
   },
   {
     name: "Nick Lewycky",
-    photo: genericPhoto,
+    photo: nickPhoto,
     description:
-      "Expert compiler engineer. Chocolatier. Ex Google, with more than 10 year tenure on the company and LLVM core contributor.",
+      "Expert compiler engineer. Chocolatier. Ex-Google, with more than 10 year tenure on the company and LLVM core contributor.",
     github: "nlewycky"
   },
   {
     name: "Aaron Turner",
-    photo: genericPhoto,
+    photo: aaronPhoto,
     description:
-      "WebAssembly frontend developer and dev advocate. Skateboarder, musician, ex-Google. Previous to joining Wasmer he created wasmbyexample and WasmBoy",
+      "WebAssembly frontend developer and dev advocate. Skateboarder, musician, Ex-Google. Previous to joining Wasmer he created wasmbyexample and WasmBoy",
     twitter: "torch2424",
     github: "torch2424"
   },
@@ -50,7 +56,7 @@ const team = [
     name: "Brandon Fish",
     photo: genericPhoto,
     description:
-      "Senior engineer with experience with language runtimes. He previously worked in Oracle-Labs in the Truffle framework, accelerating the execution of the Ruby.",
+      "Senior engineer with experience with language runtimes. He previously worked in Oracle-Labs in the Truffle framework, accelerating the execution of the Ruby runtime.",
     github: "bjfish"
   },
   {
@@ -68,23 +74,26 @@ const Home = () => (
       <title>Wasmer - About</title>
     </Head>
     <Nav />
-
-    <div className={css.hero}>
-      <h1 className={css.title}>
-        Making software universally available since 2019.
-      </h1>
-      <p>
-        Wasmer was founded to solve problems we faced ourselves every day. Now,
-        we strive to contribute to developers and companies around the world
-        reaching their full potential and we do it with a smile.
-      </p>
-      <p>
-        Our team is distributed but connected by a culture of diligence,
-        amplified by passion rather than pressure. We don’t blame, we stay kind
-        and value each other in good and in not so good times.
-      </p>
+    <div className={classnames(css.hero)}>
+      <div className={classnames(css.valuesGrid, grid.grid)} />
+      <div className={css.heroContent}>
+        <div className={css.mainHero}>
+          <h1 className={css.title}>
+            Making software universally available since 2019.
+          </h1>
+          <p>
+            Wasmer was founded to solve problems we faced ourselves every day.
+            Now, we strive to contribute to developers and companies around the
+            world reaching their full potential and we do it with a smile.
+          </p>
+          <p>
+            Our team is distributed but connected by a culture of diligence,
+            amplified by passion rather than pressure. We don’t blame, we stay
+            kind and value each other.
+          </p>
+        </div>
+      </div>
     </div>
-
     <div className={css.team}>
       <h2>Meet the team...</h2>
       <ul>
