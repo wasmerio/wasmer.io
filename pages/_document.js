333939cabea3a4d6f52af484fc7723ed992e3698
Syrus <me@syrusakbary.com>
2019-08-17 21:00:33 -0700

    Added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -37,6 +37,22 @@ class MyDocument extends Document {
           {/* End icons */}
 
           {/* <meta name="viewport" content= "width=device-width, user-scalable=yes" /> */}
+          <script dangerouslySetInnerHTML={{__html:`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '760809464311547'); // Insert your pixel ID here.
+  fbq('track', 'PageView');`}} />
+      <script dangerouslySetInnerHTML={{__html:`if(true) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  }
+  if (typeof ga === "function") {
+    ga('create', 'UA-129663180-1', 'auto', {});
+  }`}} />
         </Head>
         <head>
         <meta name="viewport" content="width=device-width"/>
