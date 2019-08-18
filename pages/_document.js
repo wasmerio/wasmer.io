d9bc986ab60baa4744aa10448655d8432943fa65
Syrus <me@syrusakbary.com>
2019-08-17 21:29:23 -0700

    Fixed Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -44,15 +44,12 @@ class MyDocument extends Document {
   document,'script','https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', '760809464311547'); // Insert your pixel ID here.
   fbq('track', 'PageView');`}} />
-      <script dangerouslySetInnerHTML={{__html:`if(true) {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  }
-  if (typeof ga === "function") {
-    ga('create', 'UA-129663180-1', 'auto', {});
-  }`}} />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129663180-1"></script>
+      <script dangerouslySetInnerHTML={{__html:`  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129663180-1');`}} />
         </Head>
         <head>
         <meta name="viewport" content="width=device-width"/>
