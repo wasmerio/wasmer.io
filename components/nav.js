1bd2388f95f16e0f412f5d51727bda1005af8779
Syrus Akbary <me@syrusakbary.com>
2020-02-20 13:00:56 -0800

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -32,7 +32,7 @@ const Nav = () => (
             <span className={css.githubAction}><img src={GithubLogo} />
             Star</span>
             <span className={css.githubStars}>
-              5,500
+              5,510
             </span>
           </a>
         </li>
