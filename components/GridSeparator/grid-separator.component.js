1ed10a2f5a0892ce3046b12e7014d249bb3c569f
julianbauer <julian@overnice.com>
2020-11-23 08:43:55 +0100

    mobile dots left padding

--- components/GridSeparator/grid-separator.component.js
+++ components/GridSeparator/grid-separator.component.js
@@ -3,7 +3,7 @@ import { ColorDots } from '../ColorDots/color-dots.component';
 
 export const GridSeparatorComponent = () => {
   return (
-    <div className="dot-background relative h-20">
+    <div className="dot-background relative h-20 ml-3 md:ml-0">
       <ColorDots items={150} />
     </div>
   );
