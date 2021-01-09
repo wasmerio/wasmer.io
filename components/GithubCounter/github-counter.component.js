367339dc37698d00bf427be36e3c514db9d88dc3
Syrus Akbary <me@syrusakbary.com>
2021-01-08 17:10:32 -0800

    Update github-counter.component.js

--- components/GithubCounter/github-counter.component.js
+++ components/GithubCounter/github-counter.component.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './github-counter.module.css';
 
 // The default number of stars to show
-const DEFAULT_NUM_STARS = 7730;
+const DEFAULT_NUM_STARS = 8000;
 
 export class GithubCounterComponent extends Component {
   constructor(props) {
