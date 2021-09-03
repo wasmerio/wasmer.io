45cd765018b8b259b352fe8c717a07240bf5ea94
Ivan Enderlin <ivan@mnt.io>
2021-09-03 12:04:19 +0200

    fix(about) Removing role.

--- components/About/Team/team.constants.js
+++ components/About/Team/team.constants.js
@@ -12,7 +12,7 @@ const team = [
   {
     name: 'Ivan Enderlin',
     description:
-      'CTO and co-founder. A versatile polyglot PhD Computer Scientist passionate about WebAssembly, with a strong open source background. Prior joining Wasmer he was working at Automattic and Mozilla.',
+      'Co-founder. A versatile polyglot PhD Computer Scientist passionate about WebAssembly, with a strong open source background. Prior joining Wasmer he was working at Automattic and Mozilla.',
     image: 'ivan.jpg',
     links: [
       { medium: 'twitter', handler: '@mnt_io' },
