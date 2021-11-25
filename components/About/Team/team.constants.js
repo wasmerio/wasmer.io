3f890484112751d1576deb020fb5d5e3ef8b4e4e
Syrus Akbary <me@syrusakbary.com>
2021-11-25 16:15:09 +0100

    Updated team page

--- components/About/Team/team.constants.js
+++ components/About/Team/team.constants.js
@@ -2,34 +2,12 @@ const team = [
   {
     name: 'Syrus Akbary',
     description:
-      'Before starting Wasmer in November 2018, Syrus Akbary was the CTO of Try.com. He’s the creator of several popular Python open-source libraries like Graphene, Promise used in 5 of top 50 Y Combinator companies.',
+      'Founder & CEO. Before starting Wasmer in November 2018, Syrus Akbary was the CTO of Try.com. He’s the creator of several popular Python open-source libraries like Graphene, Promise used in 5 of top 50 Y Combinator companies.',
     image: 'syrus.png',
     links: [
       { medium: 'twitter', handler: '@syrusakbary' },
       { medium: 'github', handler: 'syrusakbary' },
     ],
-  },
-  {
-    name: 'Ivan Enderlin',
-    description:
-      'A versatile polyglot software engineer passionate about WebAssembly. Prior joining Wasmer he was working at Automattic and Mozilla.',
-    image: 'ivan.png',
-  },
-  {
-    name: 'Mark McCaskey',
-    description:
-      'Software engineer and language lover (Mark speaks 3 different languages!). Prior joining Wasmer he created rusty-boy, a GameBoy emulator in Rust.',
-  },
-  {
-    name: 'Nick Lewycky',
-    description:
-      'Expert compiler engineer. Chocolatier. Ex-Google, with more than 10 year tenure on the company and LLVM core contributor.',
-    image: 'nick.png',
-  },
-  {
-    name: 'Heyang Zhou',
-    description:
-      'Prolific WebAssembly contributor. Before working at Wasmer he created the first Linux Subsystem based on WebAssembly: Cervus.',
   },
   {
     name: 'Wiqar Chaudry',
@@ -39,6 +17,32 @@ const team = [
       links: [
         { medium: 'twitter', handler: '@wiqarc' },
         { medium: 'github', handler: 'wchaudry' },
+      ]
+  },
+  {
+    name: 'Sébastien Chevalier',
+    description:
+      'Software Engineer. Open source enthousiast and author of box86/box64, He likes to push ARM CPU to their limits.',
+      image: 'sebastien.jpg',
+      links: [
+        { medium: 'github', handler: 'ptitSeb' },
+      ]
+  },
+  {
+    name: 'Amanieu D\'Antras',
+    description:
+      'Lead Runtime Developer. Core Rust Developer',
+      image: 'unknown.png',
+      links: [
+        { medium: 'github', handler: 'ptitSeb' },
+      ]
+  },
+  {
+    name: 'Tesi Lopez',
+    description:
+      'People and Ops. I love dancing',
+      image: 'unknown.png',
+      links: [
       ]
   },
 ];
