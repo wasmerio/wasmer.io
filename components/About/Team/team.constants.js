590017e25d1653af3c08cc81ca3a4c885e1342c8
Muhammad Tahir Nawaz <muhammadtahirnawazmnt@gmail.com>
2022-10-31 10:41:43 +0500

    Minor Fixes

--- components/About/Team/team.constants.js
+++ components/About/Team/team.constants.js
@@ -13,6 +13,7 @@ const team = [
     name: 'Ivan Enderlin',
     description:
       'A versatile polyglot PhD Computer Scientist passionate about WebAssembly, with a strong open source background. Prior joining Wasmer he was working at Automattic and Mozilla.',
+    image: 'ivan.jpg',
     links: [
       { medium: 'twitter', handler: '@mnt_io' },
       { medium: 'github', handler: 'Hywan' },
