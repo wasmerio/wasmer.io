4616361572e782ceb844274b3a6f367ec3586ebc
Muhammad Tahir Nawaz <muhammadtahirnawazmnt@gmail.com>
2022-11-02 21:08:11 +0500

    Reverted next config changes

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withReactSvg = require('next-react-svg');
 const path = require('path');
 
 module.exports = withReactSvg({
-    experimental: {
-    runtime: 'experimental-edge',
-  },
   include: path.resolve(__dirname, 'public/images'),
   webpack(config, options) {
     return config;
