626989212c3ccc41e09e174acfafea6a9e877e5f
Muhammad Tahir Nawaz <muhammadtahirnawazmnt@gmail.com>
2022-11-24 11:11:24 +0500

    Minor change

--- client.js
+++ client.js
@@ -3,5 +3,5 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'kj8f3jct', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: false // `false` if you want to ensure fresh data
+  useCdn: true // `false` if you want to ensure fresh data
 })
