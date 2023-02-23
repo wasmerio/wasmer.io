6f2ec662285652a761db38c985a87b9491583e02
Ayush Jha <ayushjha@pm.me>
2023-02-23 17:11:13 +0000

    Add proper css and js files for synax highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,21 @@ import { FooterComponent, NavComponent } from '../components';
 import '../assets/fonts/Gilroy/Gilroy.css';
 import '../assets/fonts/Zeitung/Zeitung.css';
 import '../assets/styles/index.css';
+import "prismjs/prism.js";
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-rust';
+import 'prismjs/components/prism-toml';
+import 'prismjs/components/prism-jsx';
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-shell-session";
+
+import "prismjs/themes/prism-tomorrow.css";
 
 function App({ Component, pageProps }) {
   return (
